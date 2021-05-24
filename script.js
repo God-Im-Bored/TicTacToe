27c59fb0d1eb1124edb7fcc0734158f6996255f3
Aram <aram_martin@alumni.brown.edu>
2021-05-24 18:20:40 -0500

    ADD: handleResult()

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const status = document.getElementById('game-status')
 let gameActive = true, currentPlayer = 'X', gameState = ['', '', '', '', '', '', '', '', '']
 
+const validCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], 
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
 const winBanner = () => `Player ${currentPlayer} has won!`
 const drawBanner = () => 'Game ended in a draw!'
 const whoseTurn = () => `It's ${currentPlayer}'s turn`
@@ -29,7 +40,36 @@ const handleClick = (clickedCellEvent) => {
     handleResult()
 }
 
-const handleResult = () => {}
+const handleResult = () => {
+    let won = false
+    
+    for (let i = 0; i <= 7; i++) {
+        const winCombo = validCombos[i]
+        let a = gameState[winCombo[0]], b = gameState[winCombo[1]], c = gameState[winCombo[2]]
+
+        if (a === '' || b === '' || c === '') {
+            continue
+        }
+        if (a === b && b === c) {
+            won = true
+            break
+        }
+    }
+    if (won) {
+        status.innerHTML = winBanner()
+        gameActive = false
+        return 
+    }
+
+    let draw = !gameState.includes('')
+    if (draw) {
+        status.innerHTML = drawBanner()
+        gameActive = false
+        return 
+    }
+
+    playerChange()
+}
 
 document.querySelectorAll('.cell').forEach(cell =>
     cell.addEventListener('click', handleClick))
